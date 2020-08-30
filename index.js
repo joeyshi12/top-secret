567f697985535416e268ac00f1e6db269608a87f
Joey <joeyshi12@hotmail.com>
2020-08-29 17:57:29 -0700

    lol

---
+++ index.js
@@ -0,0 +1,24 @@
+let inconsolata;
+let canvas;
+
+function preload() {
+    inconsolata = loadFont('assets/inconsolata.otf');
+}
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    canvas.parent("sketch-holder");
+    textFont(inconsolata);
+    textSize(width / 7);
+    textAlign(CENTER, CENTER);
+}
+
+function draw() {
+    resizeCanvas(windowWidth, windowHeight);
+
+    background(0);
+    let time = millis();
+    rotateX(time / 1600);
+    rotateZ(time / 1400);
+    text('joeyshi.tech', 0, 0);
+}
